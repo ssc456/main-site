874febf878993341d19952033c3471a407423e34
Ssc456 <Scott@gliocas.co.uk>
2025-07-05 23:10:48 +0100

    protect sites

--- src/admin/editors/SitesList.jsx
+++ src/admin/editors/SitesList.jsx
@@ -318,16 +318,18 @@ export default function SitesList() {
                             <path d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z" />
                           </svg>
                         </button>                      
-                        {/* Delete Button */}
-                        <button
-                          onClick={() => openDeleteConfirmation(site)}
-                          className="text-red-500 hover:text-red-700"
-                          title="Delete Site"
-                        >
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-                          </svg>
-                        </button>
+                        {/* Delete Button - Only show for non-protected sites */}
+                        {site.siteId !== 'entry-nets' && site.siteId !== 'coastal-breeze' && (
+                          <button
+                            onClick={() => openDeleteConfirmation(site)}
+                            className="text-red-500 hover:text-red-700"
+                            title="Delete Site"
+                          >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                              <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                            </svg>
+                          </button>
+                        )}
                       </div>
                     </td>
                   </tr>
